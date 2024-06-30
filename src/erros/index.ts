7e8d938a6e6dd6805189e444581866aa2ed62f78
Himanshu Mamgain <himanshum50360@gmail.com>
2024-06-30 13:12:24 +0530

    added export common file for errors

---
+++ src/erros/index.ts
@@ -0,0 +1,6 @@
+export * from "./api-errors/BadRequestError";
+export * from "./api-errors/InternalServerError";
+export * from "./api-errors/NotAcceptableError";
+export * from "./api-errors/NotAuthorizedError";
+export * from "./api-errors/NotFoundError";
+export * from "./api-errors/UnproccessableEntityError";
